29a416d2bd27d806f3c9570dc5f835bdc0ca16ee
Abhishek Jain <124518416+abhiiiish@users.noreply.github.com>
2023-12-07 23:14:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request
 import requests
-
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 import mysql.connector
 
-
 app = Flask(__name__)
-app.secret_key = '9876543210'
-
 
 db_config = {
     'host': 'localhost',
@@ -16,61 +11,42 @@ db_config = {
     'database': 'abhishek'
 }
 
-@app.route('/store', methods=['GET','POST'])
+@app.route('/store', methods=['GET', 'POST'])
 def request_get():
-
     url = 'https://callback-test-oe2t.onrender.com/store'
-
-    params = {
-        "brand_id": "656890e90866e3d38a426d4f"
-    }
-
+    params = {"brand_id": "656890e90866e3d38a426d4f"}
     headers = {"Authorization": "k5esldhz7TkrK9rYqN69RbzifYTd9u2J"}
-
-    # data = requests.get(url, params=params, headers=headers)
-
-    # if data.status_code==200:
-    #     result = data.json()
-    # else:
-    #     print("Error occurred - {}".format(data.text))
-
 
     if request.method == 'GET':
         data = requests.get(url, params=params, headers=headers)
-        brand_id = data.get('brand_id')
-        post_id = data.get('post_id')
-        caption = data.get('caption')
-        status = data.get('status')
-        generated_media = data.get('generated_media')
-        media_type = data.get('media_type')
+        if data.status_code == 200:
+            json_data = data.json()
+            brand_id = json_data.get('brand_id')
+            post_id = json_data.get('post_id')
+            caption = json_data.get('caption')
+            status = json_data.get('status')
+            generated_media = json_data.get('generated_media')
+            media_type = json_data.get('media_type')
 
-        try:
-            connection = mysql.connector.connect(**db_config)
-            cursor = connection.cursor()
+            try:
+                connection = mysql.connector.connect(**db_config)
+                cursor = connection.cursor()
 
-            sql = "INSERT INTO predis_data (brand_id, post_id, caption, status, generated_media, media_type) VALUES(%s, %s, %s, %s, %s, %s,)"
-            values = (brand_id, post_id, caption, status, generated_media, media_type)
+                sql = "INSERT INTO predis_data (brand_id, post_id, caption, status, generated_media, media_type) VALUES (%s, %s, %s, %s, %s, %s)"
+                values = (brand_id, post_id, caption, status, generated_media, media_type)
 
-            cursor.execute(sql,values)
-            connection.commit()
-        
-        except mysql.connector.Error as err:
-            return f"Error: {err}"
+                cursor.execute(sql, values)
+                connection.commit()
 
-        finally: 
-            if connection.is_connected():
-                cursor.close()
-                connection.close()
+            except mysql.connector.Error as err:
+                return f"Error: {err}"
 
-    return 
+            finally:
+                if connection.is_connected():
+                    cursor.close()
+                    connection.close()
 
-
-
-
+    return "Data stored successfully"
 
 if __name__ == '__main__':
-    port = 3000  # Use equals sign here
-    app.run(debug=True, port=port)
-
-
-
+    app.run(debug=True)
