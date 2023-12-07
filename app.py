82517534924b87115b7585c0914de439d74f5ad0
Abhishek Jain <124518416+abhiiiish@users.noreply.github.com>
2023-12-07 21:58:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/store', methods=['GET','POST'])
 def request_get():
 
-    url = 'http://127.0.0.1:5000/show'
+    url = 'https://callback-test-oe2t.onrender.com/show'
 
     params = {
         "brand_id": "656890e90866e3d38a426d4f"
@@ -16,7 +16,7 @@ def request_get():
 
     headers = {"Authorization": "k5esldhz7TkrK9rYqN69RbzifYTd9u2J"}
 
-    data = requests.get(url, params==params, headers=headers)
+    data = requests.request("GET", url, params=params, headers=headers)
 
     if data.status_code==200:
         result = data.json()
