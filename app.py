65b7fb45a7bdb84e881d2764334ccf982e3ba0ee
Abhishek Jain <124518416+abhiiiish@users.noreply.github.com>
2023-12-07 23:03:53 +0530

    Update app.py
    added sql

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template, request, jsonify
 import requests
 
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+import mysql.connector
+
 
 app = Flask(__name__)
+app.secret_key = '9876543210'
 
+
+db_config = {
+    'host': 'localhost',
+    'user': 'user_1',
+    'password': 'pass.c0m',
+    'database': 'abhishek'
+}
 
 @app.route('/store', methods=['GET','POST'])
 def request_get():
 
-    url = 'https://callback-test-oe2t.onrender.com/show'
+    url = 'https://callback-test-oe2t.onrender.com/store'
 
     params = {
         "brand_id": "656890e90866e3d38a426d4f"
@@ -16,16 +27,50 @@ def request_get():
 
     headers = {"Authorization": "k5esldhz7TkrK9rYqN69RbzifYTd9u2J"}
 
-    data = requests.request("GET", url, params=params, headers=headers)
+    # data = requests.get(url, params=params, headers=headers)
 
-    if data.status_code==200:
-        result = data.json()
+    # if data.status_code==200:
+    #     result = data.json()
+    # else:
+    #     print("Error occurred - {}".format(data.text))
 
-    return jsonify(result)
+
+    if request.method == 'GET':
+        data = requests.get(url, params=params, headers=headers)
+        brand_id = data.get('brand_id')
+        post_id = data.get('post_id')
+        caption = data.get('caption')
+        status = data.get('status')
+        generated_media = data.get('generated_media')
+        media_type = data.get('media_type')
+
+        try:
+            connection = mysql.connector.connect(**db_config)
+            cursor = connection.cursor()
+
+            sql = "INSERT INTO predis_data (brand_id, post_id, caption, status, generated_media, media_type) VALUES(%s, %s, %s, %s, %s, %s,)"
+            values = (brand_id, post_id, caption, status, generated_media, media_type)
+
+            cursor.execute(sql,values)
+            connection.commit()
+        
+        except mysql.connector.Error as err:
+            return f"Error: {err}"
+
+        finally: 
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+
+    return 
+
+
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = 3000  # Use equals sign here
+    app.run(debug=True, port=port)
+
 
 
